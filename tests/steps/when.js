1e8faaa470701eda7c47b0cfd0e78a3151f27f34
Álvaro Gómez <alvaro.gomezsastre@zooplus.com>
2019-05-10 09:40:31 +0200

    adding logs

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -8,17 +8,20 @@ async function viaHandler(functionPath, event) {
 }
 
 async function viaHttp(functionPath) {
+  console.log(`viaHTTP init for test`);
+
   const apiRoot = process.env.TEST_BASE_URL;
   const method = "GET";
 
   const url = `${apiRoot}/${functionPath}`;
 
-  console.log(url);
+  console.log(`url of resource to test ${url}`);
 
   try {
+    console.log(`trying to do HTTP request for the test`);
     const httpReq = http(method, url);
     const res = await httpReq;
-
+    console.log(`HTTP response for the test ${res}`);
     return {
       statusCode: res.status,
       body: res.body
